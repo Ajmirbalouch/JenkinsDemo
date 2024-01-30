3838ce3586dd8764ad91f5699a872a68ecfe0fc4
Ajmirbalouch <126699040+Ajmirbalouch@users.noreply.github.com>
2024-01-30 12:16:35 +0100

    Update example_test.cy.js

--- cypress/integration/example_test.cy.js
+++ cypress/integration/example_test.cy.js
@@ -3,6 +3,6 @@
 describe('Meine erste Test-Suite', () => {
   it('besucht die Startseite', () => {
     cy.visit('http://jenkins.ajmir.com/'); // Ersetzen Sie dies durch die URL Ihrer Anwendung
-    cy.contains('Willkommen'); // Ersetzen Sie 'Willkommen' durch einen Text, der auf Ihrer Seite zu finden ist
+    cy.contains('domain'); // Ersetzen Sie 'Willkommen' durch einen Text, der auf Ihrer Seite zu finden ist
   });
 });
