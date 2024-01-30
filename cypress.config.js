b5168ff12c05f491cc39f9af98e707c872443621
Ajmirbalouch <126699040+Ajmirbalouch@users.noreply.github.com>
2024-01-30 11:18:20 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,20 @@
-module.exports = {
-baseUrl: 'http://localhost:8080',
-  "integrationFolder": "cypress/integration"
-}
+const { defineConfig } = require('cypress');
 
+module.exports = defineConfig({
+  e2e: {
+    // URL des Servers, auf dem Ihre Frontend-Anwendung läuft
+    baseUrl: 'http://localhost:8080',
 
+    // Pfad zu den Integrationstestdateien
+    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+
+    // Weitere E2E-spezifische Konfigurationen können hier hinzugefügt werden, falls erforderlich
+  },
+
+  // Hier können Sie globale oder gemeinsame Konfigurationen hinzufügen, die sowohl für Komponententests als auch für E2E-Tests gelten
+  // Beispiel:
+  // viewportWidth: 1280,
+  // viewportHeight: 720,
+
+  // Andere globale oder gemeinsame Konfigurationen...
+});
